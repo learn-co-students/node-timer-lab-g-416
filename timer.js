8995c5305de1060421755d808b80d9dc55a1a566
github username <juliannne@gmail.com>
2018-05-14 15:55:28 -0400

    Done.

--- timer.js
+++ timer.js
@@ -1,21 +1,43 @@
-sec = process.argv[2]
-
-if (sec.split("").includes("s")) {
-  global.setInterval(timer(sec));
+var timeLeft = 0
+var argv = process.argv
+if (!argv[2]) {
+  console.log('Please provide an argument, e.g., 5s or 1min.')
+  process.exit(1)
+} else if (argv[2].indexOf('s')>-1) {
+  timeLeft = parseInt(argv[2].slice(0, -1), 10)
+} else if (argv[2].indexOf('min')>-1) {
+  timeLeft = parseInt(argv[2].slice(0, -3), 10) * 60
 } else {
-  global.setInterval(timeMin(sec));
+  console.log('Please provide a valid argument, e.g., 5s or 1min.')
+  process.exit(1)
 }
 
-
-function timer(sec) {
-  for (let i = parseInt(sec) - 1; i >= 0; i--) {
-    console.log(`Left: ${i}s`)
+var interval = setInterval(()=>{
+  timeLeft --
+  console.log(`Left: ${timeLeft}s`)
+  if (timeLeft == 0) {
+    clearInterval(interval)
+    process.exit(0)
   }
-}
+}, 1000)
 
-function timeMin(sec) {
-  seconds = parseInt(sec) * 60
-  for (let i = seconds - 1; i >= 0; i--) {
-    console.log(`Left: ${i}s`)
-  }
-}
+// sec = process.argv[2]
+//
+// if (sec.split("").includes("s")) {
+//   global.setInterval(timer(sec));
+// } else {
+//   global.setInterval(timeMin(sec));
+// }
+//
+// function timer(sec) {
+//   for (let i = parseInt(sec) - 1; i >= 0; i--) {
+//     console.log(`Left: ${i}s`)
+//   }
+// }
+//
+// function timeMin(sec) {
+//   seconds = parseInt(sec) * 60
+//   for (let i = seconds - 1; i >= 0; i--) {
+//     console.log(`Left: ${i}s`)
+//   }
+// }
