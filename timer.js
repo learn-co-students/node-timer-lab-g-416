d0fe8da7b6350b1392306dcff8a978410160247d
cdhenry <cdhenry@gmail.com>
2018-07-08 18:18:11 -0400

    Done.

--- timer.js
+++ timer.js
@@ -1,19 +1,24 @@
-process.argv.shift()
-process.argv.shift()
-
-const timeString = process.argv[0]
+const timeString = process.argv[2]
 const unitsIndex = timeString.search(/[a-zA-Z]/)
 var time = parseInt(timeString.slice(0, unitsIndex))
 let units = timeString.slice(unitsIndex)
 
-if (units[0] === 'm') {
+if (units[0] === 's'){
+  time = time
+}else if(units[0] === 'm') {
   time = time*60
 } else if (units[0] === 'h'){
   time = time*60*60
+} else {
+  console.log('Please provide a valid argument, e.g., 5s or 1min.')
+  process.exit(1)
 }
 
 const interval = setInterval(() => {
   time -= 1
   console.log('Left: ' + time + 's')
-  if (time === 0) {clearInterval(interval)}
+  if (time === 0) {
+    clearInterval(interval)
+    process.exit(0)
+  }
 }, 1000);
