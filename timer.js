5b6a934557cb633b710f4154505a95620148dd13
Michael Pieschel <mpieschel39@gmail.com>
2019-07-20 03:29:32 +0000

    Done.

--- timer.js
+++ timer.js
@@ -1,22 +1,22 @@
 let timeLeft = 0;
 let argv = process.argv;
 if (!argv[2]) {
-  console.log('Error. Please, enter time (10s or 5min).')
-  process.exit(1)
+  console.log('Error. Please, enter time (10s or 5min).');
+  process.exit(1);
 } else if (argv[2].indexOf('s') > -1) {
-  timeLeft = parseInt(argv[2].slice(0, -1), 10)
+  timeLeft = parseInt(argv[2].slice(0, -1), 10);
 } else if (argv[2].indexOf('min') > -1) {
-  timeLeft = parseInt(argv[2].slice(0, -3), 10) * 60
+  timeLeft = parseInt(argv[2].slice(0, -3), 10) * 60;
 } else {
-  console.log('Error. Please, enter time (10s or 5min).')
-  process.exit(1)
+  console.log('Error. Please, enter time (10s or 5min).');
+  process.exit(1);
 }
 
 let interval = setInterval(()=>{
-  timeLeft --
-  console.log(`Left: ${timeLeft}s`)
+  timeLeft--;
+  console.log(`Left: ${timeLeft}s`);
   if (timeLeft == 0) {
-    clearInterval(interval)
-    process.exit(0)
+    clearInterval(interval);
+    process.exit(0);
   }
-}, 1000)
+}, 1000);
