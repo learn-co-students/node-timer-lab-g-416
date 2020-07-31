f638a547c81ff6e8ccda0220ce8e03a94c939de7
treuther <tracyreuther@gmail.com>
2020-07-31 13:02:44 -0500

    Done.

--- timer.js
+++ timer.js
@@ -4,29 +4,27 @@
 // create switch for s and min
 
 process.argv.shift()
-process.argv.shift()
+var time = parseTime(process.argv[2])
 
-// create initial variable
-// variable equals parsing funciton that passes in user argv
-const time = parseTime(process.argv[0])
+
 
 function parseTime(string) {
-    //create variable that subtracts given number to find 's' or 'n'
-    let indicator = string.charAt(string.length - 1)
+    var indicator = string.charAt(string.length - 1)
     if (indicator == "s") {
-        return parseInt(string.substr(0, string.length-1), 10)
-    } else if (indicator == 'n') {
-        return parseInt(string.substr(0, string.length-1), 10) * 60
+        return parseInt(string.substring(0, string.length-1), 10)
+    } else if (indicator == "n"){
+        return parseInt(string.substring(0, string.length-1), 10) * 60
     } else {
         console.log("Please pass in the arguments in the following format: 5s or 3min")
         process.exit()
     }
+    
 }
 
-setInterval(function() {
+setInterval(function(){
     time--
     console.log("Left: " + time + "s")
-    if (time <= 0) {
+    if(time <= 0){
         process.exit()
     }
 }, 1000)
