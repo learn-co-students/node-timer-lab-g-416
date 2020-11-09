0ebfd91e740579516f3ceb5a214645c41f3d8969
Kelly Sciandra <kellysciandradev@icloud.com>
2020-11-09 14:28:08 -0500

    Done.

--- timer.js
+++ timer.js
@@ -2,14 +2,14 @@ process.argv.shift()
 process.argv.shift()
 
 let number = parseInt(process.argv, 10)
-let bumber = 60
+let minutes = parseInt(process.argv, 10) * 60
 
 function intervalFunc() {
   if (number === 0) {
     process.exit()
   } else if ( process.argv.join("")[1] === 'm') {
-    bumber = bumber -1
-    console.log(`Left: ${bumber}s`)
+    minutes = minutes -1
+    console.log(`Left: ${minutes}s`)
 
   } else {
     number = number - 1
